a00f7c9a270813f7792388cb6cb5785a9d654e79
Jan Pres <janpres2011@gmail.com>
2025-03-21 16:09:10 +0000

    removed runner.js cause too many errors

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -482,36 +482,11 @@ async function saveToDatabase(domain, coupons) {
 
 async function main() {
   // Define all alphabet letters including special characters
-  const letters = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "0",
-  ];
-
+  const letters = process.env.LETTERS
+    ? process.env.LETTERS.split(",")
+    : process.argv.length > 2
+    ? process.argv[2].split(",")
+    : [];
   let totalSuccessCount = 0;
   let totalErrorCount = 0;
 
